6725ee6844da53305ef982421312701d28be8b60
Kundan <kkscwcd@gmail.com>
2019-01-08 16:04:27 +0530

    Initial commit

---
+++ app/src/main/java/lueorganisation/winmall/via/touristapp/SplashActivity.java
@@ -0,0 +1,20 @@
+package lueorganisation.winmall.via.touristapp;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 3000);
+    }
+}
